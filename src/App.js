c185b92a80d891f3da486b6f0b1b944122c027b1
Mateusz Kotwiński <mkotwinski98@gmail.com>
2020-04-08 16:53:45 +0200

    Import Form and Result

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+
+import Form from './components/Form/Form';
+import Result from './components/Result/Result';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Form />
+        <Result />
       </div>
     );
   }
