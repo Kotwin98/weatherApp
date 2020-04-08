70aa25527b11b8a4d642c4371235f8dac6ad725b
Mateusz Kotwiński <mkotwinski98@gmail.com>
2020-04-08 17:20:04 +0200

    Add state and handleCitySubmit method

--- src/App.js
+++ src/App.js
@@ -5,10 +5,41 @@ import Result from './components/Result/Result';
 import './App.css';
 
 class App extends Component {
+  state = {
+    value: "",
+    date: "",
+    city: "",
+    sunrise: "",
+    sunset: "",
+    temp: "",
+    pressure: "",
+    wind: "",
+    err: ""
+  };
+
+  handleCitySubmit = e => {
+    e.preventDefault();
+    const API = `http://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&appid=eb83d66ce254c055b493127ed3693ace&units=metric`;
+
+    fetch(API)
+    .then(response => {
+      if (response.ok) {
+        return response;
+      }
+      throw Error('Something went wrong');
+    })
+    .then(response => response.json())
+    .then(result => console.log(result))
+    .catch(err => console.log(err));
+  };
+
   render() {
     return (
       <div className="App">
-        <Form />
+        <Form 
+          value={this.state.value}
+          submit={this.handleCitySubmit}
+        />
         <Result />
       </div>
     );
