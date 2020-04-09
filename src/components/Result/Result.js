214adcc5dd5593e2b1c95ec4b2aa370558d8e04a
Mateusz Kotwiński <mkotwinski98@gmail.com>
2020-04-09 10:13:07 +0200

    Display weather information

--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -1,10 +1,27 @@
 import React from 'react';
 
-const Result = () => {
+const Result = (props) => {
+    const {
+        date,
+        city,
+        sunrise,
+        sunset,
+        temp,
+        pressure,
+        wind,
+        err
+    } = props.weather;
+
     return (
-        <div>
-            <p>Weather display</p>
-        </div>
+        <>
+            <div>Date: {date}</div>
+            <div>City: {city}</div>
+            <div>Temp: {temp}</div>
+            <div>Sunrise: {sunrise}</div>
+            <div>Sunset: {sunset}</div>
+            <div>Pressure: {pressure}</div>
+            <div>Wind: {wind}</div>
+        </>
     );
 };
 
