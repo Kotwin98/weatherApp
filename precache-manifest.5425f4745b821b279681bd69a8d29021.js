b518e6185613d5f0759d6d5c3052562e5b83f2ee
Mateusz Kotwiński <mkotwinski98@gmail.com>
2020-04-10 08:52:37 +0200

    Updates

---
+++ precache-manifest.5425f4745b821b279681bd69a8d29021.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "381ed7e278950fa028366c13f5ab83a8",
+    "url": "/weatherApp/index.html"
+  },
+  {
+    "revision": "ef8d9c28921c40bb1540",
+    "url": "/weatherApp/static/css/main.1cc8590a.chunk.css"
+  },
+  {
+    "revision": "b0525cea0ccee750d16b",
+    "url": "/weatherApp/static/js/2.75cedfe0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weatherApp/static/js/2.75cedfe0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef8d9c28921c40bb1540",
+    "url": "/weatherApp/static/js/main.ff9f6f93.chunk.js"
+  },
+  {
+    "revision": "fd8f53948cfb6005b5f2",
+    "url": "/weatherApp/static/js/runtime-main.ff98399e.js"
+  }
+]);
